43bbcb7eac1d4b8aa50fc465d8cd7c1ee396b5f0
Waihon Yew <yewwaihon@gmail.com>
2022-05-05 23:55:14 +0800

    Initial Commit

---
+++ BetterRest2/BetterRest2App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest2App.swift
+//  BetterRest2
+//
+//  Created by Waihon Yew on 05/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
