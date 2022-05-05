a1ef3e056252b5ce8529dd4af154e52683f20516
Waihon Yew <yewwaihon@gmail.com>
2022-05-06 00:02:28 +0800

    Create a stepper view and bind it to a variable

--- BetterRest2/ContentView.swift
+++ BetterRest2/ContentView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+
     var body: some View {
-        Text("Hello, world!")
+        Stepper("\(sleepAmount) hours", value: $sleepAmount)
             .padding()
     }
 }
