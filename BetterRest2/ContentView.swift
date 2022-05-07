c6ed0e1e9480a9113846b664c6a909922bd4eff0
Waihon Yew <yewwaihon@gmail.com>
2022-05-07 11:02:15 +0800

    Display the sleep amount with just enough decimal places if applicable

--- BetterRest2/ContentView.swift
+++ BetterRest2/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
 
     var body: some View {
-        Stepper("\(sleepAmount) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             .padding()
     }
 }
