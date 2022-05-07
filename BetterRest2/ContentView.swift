4569f9120503c955e8fffe9abe69edccbdf90e4e
Waihon Yew <yewwaihon@gmail.com>
2022-05-07 11:22:09 +0800

    Bind DatePicker with default options to a date variable

--- BetterRest2/ContentView.swift
+++ BetterRest2/ContentView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
 
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            .padding()
+        Form {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                .padding()
+
+            DatePicker("Date", selection: $wakeUp)
+        }
     }
 }
 
