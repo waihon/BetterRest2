d9a52658fddc3912e5d0556f38291d7b4bdec1f5
Waihon Yew <yewwaihon@gmail.com>
2022-05-11 23:02:00 +0800

    Add 3 properties that let us store the info for UI controls later on

--- BetterRest2/ContentView.swift
+++ BetterRest2/ContentView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct OverviewContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var wakeUp2 = Date.now
