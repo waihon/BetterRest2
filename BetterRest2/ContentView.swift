dbfb47feaafbb87e08fc5b6554d0ee7c22aff04c
Waihon Yew <yewwaihon@gmail.com>
2022-05-11 23:04:37 +0800

    Add label and control for wake up time

--- BetterRest2/ContentView.swift
+++ BetterRest2/ContentView.swift
@@ -13,8 +13,16 @@ struct ContentView: View {
     @State private var coffeeAmount = 1
 
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+            }
             .padding()
+        }
     }
 }
 
